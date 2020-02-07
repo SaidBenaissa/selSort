2c98a1b97939a85ecbea349be29562832a08c4d0
SaidBenaissa <d.sbenaissa@gmail.com>
2020-02-07 18:17:52 +0100

    Selection sort alg time complexity

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 class SelectionSort{
 public:
     void selSort(int A[], int n){
-    for (int i = 0; i <n-1 ; ++i) {
+    for (int i = 0; i <n-2 ; ++i) { // Time complexity for this i loop = C1* (n-1)  then O(n-1) times
 
         int iMin =i;
-        for (int j = i + 1; j < n ; ++j) {
+        //----- time complexity for this block: C2*[(n-1)+(n-2)+ .....+1] = C2 * n(n-1)/2 then O(n^2) time
+        for (int j = i + 1; j < n-1 ; ++j) {
             if (A[j] < A[iMin]){
                 iMin = j;
             }
         }
+       //-------------------------------------------------------------------------------------------
         int temp = A[i];
         A[i] = A[iMin];
         A[iMin] = temp;
@@ -25,3 +27,5 @@ int main() {
     }
     return 0;
 }
+
+// Time complexity of Selection sort algorithm is BigO of n^2  = O(n^2)
