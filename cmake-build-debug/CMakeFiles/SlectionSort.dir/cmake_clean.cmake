800deba01ae66327c42cffd1ef9bdc8e8eae09ff
SaidBenaissa <d.sbenaissa@gmail.com>
2020-02-07 16:59:31 +0100

    first commit 1

---
+++ cmake-build-debug/CMakeFiles/SlectionSort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SlectionSort.dir/main.cpp.o"
+  "SlectionSort"
+  "SlectionSort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SlectionSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void selSort(int A[], int n){
+    for (int i = 0; i <n-1 ; ++i) {
+
+        int iMin =i;
+        for (int j = i + 1; j < n ; ++j) {
+            if (A[j] < A[iMin]){
+                iMin = j;
+            }
+        }
+        int temp = A[i];
+        A[i] = A[iMin];
+        A[iMin] = temp;
+    }
+}
+int main() {
+    int A[] = {2,7,4,1,5,3};
+    selSort(A,5);
+    for (int i = 0; i <6; ++i) {
+        std::cout << A[i] << " ";
+    }
+    return 0;
+}
